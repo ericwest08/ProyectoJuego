5a9d51e4185f8ca60b6893b072f67d4aafec4606
Joseph <joseph.zandueta@estudiant.upc.edu>
2020-01-19 19:58:10 +0100

    Camdiado por services en vez de Service

--- src/main/java/edu/upc/dsa/Main.java
+++ src/main/java/edu/upc/dsa/Main.java
@@ -20,7 +20,7 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in edu.upc.dsa package
-        final ResourceConfig rc = new ResourceConfig().packages("edu.upc.eetac.dsa.services");
+        final ResourceConfig rc = new ResourceConfig().packages("edu.upc.dsa.services");
 
         rc.register(io.swagger.jaxrs.listing.ApiListingResource.class);
         rc.register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
